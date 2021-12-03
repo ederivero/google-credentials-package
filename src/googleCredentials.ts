e4186dd66dbf9a6c06b5a28bf89e45bc1adad023
eduardo de rivero <eduardomanrique@ravn.co>
2021-12-03 16:00:59 -0500

    chore: implement function to nestjs and expressjs

--- src/googleCredentials.ts
+++ src/googleCredentials.ts
@@ -1,12 +1,15 @@
 import { GOOGLE_CREDENTIALS } from "./types"
+import stackdriver from "pino-stackdriver"
 
-export function googleApplicationCredentials(
-  env = "GOOGLE_APPLICATION_CREDENTIALS",
-) {
-  if (!process.env[env]) {
+function validateCredentials(google_credentials_env: string) {
+  const env = process.env[google_credentials_env]
+
+  if (!env) {
     throw new Error("Unable to find Google Credentials")
   }
-  const credentials: GOOGLE_CREDENTIALS = JSON.parse(process.env[env] ?? "")
+
+  const credentials: GOOGLE_CREDENTIALS = JSON.parse(env)
+
   if (
     !credentials.type ||
     !credentials.project_id ||
@@ -21,5 +24,78 @@ export function googleApplicationCredentials(
   ) {
     throw new Error("Unable to find Google Credentials")
   }
+
   return credentials
+}
+
+export function NestJsLogger(
+  google_credentials_env: string = "GOOGLE_APPLICATION_CREDENTIALS",
+) {
+  let pinoHttp
+  if (process.env.NODE_ENV === "production") {
+    const credentials = validateCredentials(google_credentials_env)
+
+    pinoHttp = [
+      { level: "info" },
+      stackdriver.createWriteStream({
+        projectId: credentials.project_id,
+        credentials: {
+          client_email: credentials.client_email,
+          private_key: credentials.private_key,
+        },
+      }),
+    ]
+  } else {
+    pinoHttp = [
+      {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: true,
+            levelFirst: true,
+          },
+        },
+      },
+    ]
+  }
+
+  return pinoHttp
+}
+
+export function ExpressLogger(
+  google_credentials_env: string = "GOOGLE_APPLICATION_CREDENTIALS",
+) {
+  if (process.env.NODE_ENV === "production") {
+    const credentials = validateCredentials(google_credentials_env)
+
+    return {
+      options: {
+        level: "info",
+      },
+      stream: stackdriver.createWriteStream({
+        projectId: credentials.project_id,
+        credentials: {
+          client_email: credentials.client_email,
+          private_key: credentials.private_key,
+        },
+        logName: "heimdall-webhook-proxies",
+      }),
+    }
+  } else {
+    return {
+      options: {
+        level: "debug",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: true,
+            levelFirst: true,
+          },
+        },
+      },
+      stream: null,
+    }
+  }
 }
